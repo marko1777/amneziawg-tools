66de01f21196befe6676cdd09c637224ff7f8851
Mark Puha <p.mark95@gmail.com>
2025-06-14 15:02:32 +0200

    fix: formatting

--- src/uapi/openbsd/net/if_wg.h
+++ src/uapi/openbsd/net/if_wg.h
@@ -7,11 +7,12 @@
 #ifndef __IF_WG_H__
 #define __IF_WG_H__
 
-#include <sys/errno.h>
 #include <sys/limits.h>
+#include <sys/errno.h>
 
 #include <net/if.h>
 #include <netinet/in.h>
+
 
 /*
  * This is the public interface to the WireGuard network interface.
@@ -24,56 +25,52 @@
 #define SIOCSWG _IOWR('i', 210, struct wg_data_io)
 #define SIOCGWG _IOWR('i', 211, struct wg_data_io)
 
-#define a_ipv4 a_addr.addr_ipv4
-#define a_ipv6 a_addr.addr_ipv6
+#define a_ipv4	a_addr.addr_ipv4
+#define a_ipv6	a_addr.addr_ipv6
 
-struct wg_aip_io
-{
-    sa_family_t a_af;
-    int         a_cidr;
-    union wg_aip_addr
-    {
-        struct in_addr  addr_ipv4;
-        struct in6_addr addr_ipv6;
-    } a_addr;
+struct wg_aip_io {
+    sa_family_t	 a_af;
+    int		 a_cidr;
+    union wg_aip_addr {
+        struct in_addr		addr_ipv4;
+        struct in6_addr		addr_ipv6;
+    }		 a_addr;
 };
 
-#define WG_PEER_HAS_PUBLIC (1 << 0)
-#define WG_PEER_HAS_PSK (1 << 1)
-#define WG_PEER_HAS_PKA (1 << 2)
-#define WG_PEER_HAS_ENDPOINT (1 << 3)
-#define WG_PEER_REPLACE_AIPS (1 << 4)
-#define WG_PEER_REMOVE (1 << 5)
-#define WG_PEER_UPDATE (1 << 6)
+#define WG_PEER_HAS_PUBLIC		(1 << 0)
+#define WG_PEER_HAS_PSK			(1 << 1)
+#define WG_PEER_HAS_PKA			(1 << 2)
+#define WG_PEER_HAS_ENDPOINT	(1 << 3)
+#define WG_PEER_REPLACE_AIPS	(1 << 4)
+#define WG_PEER_REMOVE			(1 << 5)
+#define WG_PEER_UPDATE			(1 << 6)
 
-#define p_sa p_endpoint.sa_sa
-#define p_sin p_endpoint.sa_sin
-#define p_sin6 p_endpoint.sa_sin6
+#define p_sa		p_endpoint.sa_sa
+#define p_sin		p_endpoint.sa_sin
+#define p_sin6		p_endpoint.sa_sin6
 
-struct wg_peer_io
-{
-    int      p_flags;
-    int      p_protocol_version;
-    uint8_t  p_public[WG_KEY_LEN];
-    uint8_t  p_psk[WG_KEY_LEN];
-    uint16_t p_pka;
-    union wg_peer_endpoint
-    {
-        struct sockaddr     sa_sa;
-        struct sockaddr_in  sa_sin;
-        struct sockaddr_in6 sa_sin6;
-    } p_endpoint;
-    uint64_t         p_txbytes;
-    uint64_t         p_rxbytes;
-    struct timespec  p_last_handshake; /* nanotime */
-    size_t           p_aips_count;
-    struct wg_aip_io p_aips[];
+struct wg_peer_io {
+    int			p_flags;
+    int			p_protocol_version;
+    uint8_t			p_public[WG_KEY_LEN];
+    uint8_t			p_psk[WG_KEY_LEN];
+    uint16_t		p_pka;
+    union wg_peer_endpoint {
+        struct sockaddr		sa_sa;
+        struct sockaddr_in	sa_sin;
+        struct sockaddr_in6	sa_sin6;
+    }			p_endpoint;
+    uint64_t		p_txbytes;
+    uint64_t		p_rxbytes;
+    struct timespec		p_last_handshake; /* nanotime */
+    size_t			p_aips_count;
+    struct wg_aip_io	p_aips[];
 };
 
-#define WG_INTERFACE_HAS_PUBLIC (1 << 0)
-#define WG_INTERFACE_HAS_PRIVATE (1 << 1)
-#define WG_INTERFACE_HAS_PORT (1 << 2)
-#define WG_INTERFACE_HAS_RTABLE (1 << 3)
+#define WG_INTERFACE_HAS_PUBLIC    (1 << 0)
+#define WG_INTERFACE_HAS_PRIVATE   (1 << 1)
+#define WG_INTERFACE_HAS_PORT      (1 << 2)
+#define WG_INTERFACE_HAS_RTABLE    (1 << 3)
 #define WG_INTERFACE_REPLACE_PEERS (1 << 4)
 #define WG_INTERFACE_DEVICE_HAS_JC (1 << 5)
 #define WG_INTERFACE_DEVICE_HAS_JMIN (1 << 6)
@@ -94,25 +91,24 @@ struct wg_peer_io
 #define WG_INTERFACE_DEVICE_HAS_J3 (1 << 21)
 #define WG_INTERFACE_DEVICE_HAS_ITIME (1 << 22)
 
-struct wg_interface_io
-{
-    uint16_t          i_flags;
-    in_port_t         i_port;
-    int               i_rtable;
-    uint8_t           i_public[WG_KEY_LEN];
-    uint8_t           i_private[WG_KEY_LEN];
-    size_t            i_peers_count;
-    struct wg_peer_io i_peers[];
+struct wg_interface_io {
+    uint16_t		i_flags;
+    in_port_t		i_port;
+    int				i_rtable;
+    uint8_t			i_public[WG_KEY_LEN];
+    uint8_t			i_private[WG_KEY_LEN];
+    size_t			i_peers_count;
+    struct wg_peer_io	i_peers[];
 
-    uint16_t i_junk_packet_count;
-    uint16_t i_junk_packet_min_size;
-    uint16_t i_junk_packet_max_size;
-    uint16_t i_init_packet_junk_size;
-    uint16_t i_response_packet_junk_size;
-    uint32_t i_init_packet_magic_header;
-    uint32_t i_response_packet_magic_header;
-    uint32_t i_underload_packet_magic_header;
-    uint32_t i_transport_packet_magic_header;
+    uint16_t 			i_junk_packet_count;
+    uint16_t 			i_junk_packet_min_size;
+    uint16_t 			i_junk_packet_max_size;
+    uint16_t 			i_init_packet_junk_size;
+    uint16_t 			i_response_packet_junk_size;
+    uint32_t 			i_init_packet_magic_header;
+    uint32_t 			i_response_packet_magic_header;
+    uint32_t 			i_underload_packet_magic_header;
+    uint32_t 			i_transport_packet_magic_header;
 
     uint8_t* i_i1;
     uint8_t* i_i2;
@@ -125,11 +121,10 @@ struct wg_interface_io
     uint32_t i_itime;
 };
 
-struct wg_data_io
-{
-    char   wgd_name[IFNAMSIZ];
-    size_t wgd_size; /* total size of the memory pointed to by wgd_interface */
-    struct wg_interface_io* wgd_interface;
+struct wg_data_io {
+    char	 		 wgd_name[IFNAMSIZ];
+    size_t			 wgd_size;	/* total size of the memory pointed to by wgd_interface */
+    struct wg_interface_io	*wgd_interface;
 };
 
 #endif /* __IF_WG_H__ */
